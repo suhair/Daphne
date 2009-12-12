03036b04483183f5517997769c459d2144106f95
Suhair Hassan E <suhairhassan@ispg.in>
2009-12-12 17:29:33 +0530

    Entity model and first passing test added

---
+++ DaphneDomainTests/UserFixture.cs
@@ -0,0 +1,52 @@
+﻿using DaphneDomain;
+using MbUnit.Framework;
+using System.Linq;
+using NDbUnit.Core;
+using NDbUnit.Core.SqlClient;
+
+
+namespace DaphneDomainTests
+{
+    public class FixtureBase
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            WipeOutDatabase();
+        }
+
+        private static void WipeOutDatabase()
+        {
+            SqlDbUnitTest testDb = new SqlDbUnitTest(@"Data Source=ISPG341\SQLEXPRESS;Initial Catalog=Daphne;Integrated Security=True");
+            testDb.ReadXmlSchema(@"..\..\database.xsd");
+            testDb.PerformDbOperation(DbOperationFlag.DeleteAll);
+        }
+    }
+
+    [TestFixture]
+    public class UserFixture : FixtureBase
+    {
+        [Test]
+        public void should_be_able_to_save_user()
+        {
+            using (var context = new DaphneEntities())
+            {
+                
+
+                User user = new User();
+                user.UserName = "suhair";
+                user.Password = "password";
+                context.AddToUsers(user);
+                context.SaveChanges();
+
+                var saveduser = ((from u in context.Users
+                  select u)).FirstOrDefault();
+
+                Assert.IsNotNull(saveduser, "The user object was failed to save in database");
+
+            }
+            
+
+        }
+    }
+}
